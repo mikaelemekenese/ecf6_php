594498600dbe2f63e3b3fc0f80405b352088d468
mikaelemekenese <m.mekenese@cci-formation.nc>
2021-06-29 13:18:26 +1100

    modification posts et css

--- posts.php
+++ posts.php
@@ -11,35 +11,64 @@
         echo "<style>#logout { display:none; }</style>";
         echo "<style>#post-create { display:none; }</style>";
     }
+    $pdo = pdo_connect_mysql();
+
+    $pdo_stmt = $pdo->prepare(' SELECT  post.id, 
+                                        post.titre, 
+                                        post.date_publication, 
+                                        admins.nom_admin, 
+                                        categories.nom_cat,
+                                        tag.nom_tag 
+                                FROM    post
+                                LEFT JOIN admins 
+                                ON      post.admin_id = admins.id
+                                LEFT JOIN categories
+                                ON      post.cat_id = categories.id
+                                LEFT JOIN tag
+                                ON      post.tag_id = tag.id');
+                                
+    $pdo_stmt->execute();
+
+    $posts = $pdo_stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<?php echo template_header('Accueil'); ?>
+<?php echo template_header('Liste des posts'); ?>
 
-    <?php 
+    <div class="content read" style="width:90%;">
 
-    $pdo = pdo_connect_mysql();
-    
-    $req = $pdo->query('SELECT id, titre, contenu, DATE_FORMAT(date_publication, \'%d/%m/%Y\') AS date_p FROM post ORDER BY date_publication DESC LIMIT 0, 5'); 
+        <div><h2>Liste des posts du blog</h2> 
+        <span><a href="post-create.php" class="add"><i class="fas fa-plus-square fa-xs"></i></a></span></div>
 
-    while ($donnees = $req->fetch()) {
-    ?>
-
-        <div class="container">
-            <div class="row">
-                <article>
-                    <h3>
-                        <?php echo htmlspecialchars($donnees['titre']); ?><br>
-                        <em>le <?php echo $donnees['date_p']; ?></em><br>
-                    </h3>
-
-                    <p>
-                        <?php echo htmlspecialchars($donnees['contenu']); ?><br>
-                        <em><a href="comments.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a></em>
-                    </p>
-                </article>
-            </div>
-        </div>
-    
-    <?php } $req->closeCursor(); ?>
+        <table class="table">
+            <thead>
+                <tr>
+                    <td>#</td>
+                    <td>Titre</td>
+                    <td>Date de publication</td>
+                    <td>Admin</td>
+                    <td>Catégorie</td>
+                    <td>Tag</td>
+                    <td></td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($posts as $post) : ?>
+                <tr>
+                    <td><?php echo $post["id"] ?></td>
+                    <td><?php echo $post["titre"] ?></td>
+                    <td><?php echo $post["date_publication"] ?></td>
+                    <td><?php echo $post["nom_admin"] ?></td>
+                    <td><?php echo $post["nom_cat"] ?></td>
+                    <td><?php echo $post["nom_tag"] ?></td>
+                    <td class="actions">
+                        <a href="comments.php?billet=<?php echo $post['id']; ?>" class="read-more"><i class="fas fa-book fa-xs"></i></a>
+                        <a href="post-update.php?id=<?php echo $post["id"] ?>" class="edit"><i class="fas fa-pen fa-xs"></i></a>
+                        <a href="post-delete.php?id=<?php echo $post["id"] ?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 
 <?php echo template_footer(); ?>
